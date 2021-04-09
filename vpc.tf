54e8577130dc6ac838c2d931dec9f5211ab27f4c
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-04-10 03:35:31 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -128,9 +128,9 @@ resource "aws_route_table" "private" {
 # add a nat gateway to each private subnet's route table
 resource "aws_route" "private_nat_gateway_route" {
   count = length(var.azs)
-  route_table_id = [element(aws_route_table.private.*.id, count.index)]
+  route_table_id = element(aws_route_table.private.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
   depends_on = ["aws_route_table.private"]
-  nat_gateway_id = [element(aws_nat_gateway.demo.*.id, count.index)]
+  nat_gateway_id = element(aws_nat_gateway.demo.*.id, count.index)
 }
 
