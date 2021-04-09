c5c995e5c2931ffe61b3bc5a0fd8518a8892f93a
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-04-10 02:57:20 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,12 +1,12 @@
 # Put the following two keys into shell or env variables
 
-#variable "aws_access_key" {
-#  description = "AWS access key, must pass on command line using -var"
-#}
+variable "aws_access_key" {
+  description = "AWS access key, must pass on command line using -var"
+}
 
-#variable "aws_secret_key" {
-#  description = "AWS secret access key, must pass on command line using -var"
-#}
+variable "aws_secret_key" {
+  description = "AWS secret access key, must pass on command line using -var"
+}
 
 variable "aws_region" {
   description = "US EAST Virginia"
