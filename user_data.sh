9b73f04dbcb61ad26b22d2a072962f3cc0335247
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-04-10 02:21:53 +0530

    init

---
+++ user_data.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# user_data scripts automatically execute as root user, 
+# so, no need to use sudo
+sudo apt update -y
+sudo apt upgrade -y
+
+#Install Prerequisite Packages
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
+
+#Add the Docker Repositories
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update -y
+
+# install docker community edition
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+systemctl start docker
+systemctl enable docker
+
+#install ansible
+echo -e " " | sudo apt-add-repository ppa:ansible/ansible
+
+sudo apt install ansible
+
+# pull nginx image
+#docker pull nginx:latest
+cd ./deployment/  || exit
+ansible-playbook prod-web.yml --tags=ansible-role-docker -e "githubuser=Shashankreddysunkara" -e "githubpassword=<change-me>"
+ansible-playbook prod-web.yml --tags=create-role -e "githubuser=Shashankreddysunkara" -e "githubpassword=<change-me>"
+
+## pull nginx-php image
+#docker pull webdevops/php-nginx:ubuntu-18.04
+#
+## run container with port mapping - host:container
+#docker run -d -p 80:80 --name nginx-php webdevops/php-nginx:ubuntu-18.04
+#docker exec -it nginx-php bash -c "echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html"
