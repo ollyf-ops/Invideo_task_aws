98f47a8ad5bda17a1f86d4f008ef40edecb4a9cb
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-04-12 00:39:13 +0530

    Update db_instance.tf

--- db_instance.tf
+++ db_instance.tf
@@ -50,7 +50,7 @@ resource "aws_db_instance" "db" {
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds_subnet_group-${count.index}"
   count      = length(var.azs)
-  subnet_ids = ["${aws_subnet.private.*.id}"]
+  subnet_ids = element(split(",", join(",", aws_subnet.private.*.id)), count.index)
 
 }
 
